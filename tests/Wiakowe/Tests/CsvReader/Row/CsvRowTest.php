1dcec0f7126049b00d20925f06d5844f69736730
Roger Llopart Pla <lumbendil@gmail.com>
2012-06-15 18:20:44 +0200

    Added the skeleton of the tests that should be implemented.

---
+++ tests/Wiakowe/Tests/CsvReader/Row/CsvRowTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Wiakowe\Tests\CsvReader\Row;
+
+class CsvRowTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {}
+
+    public function testGetRowPosition()
+    {}
+
+    public function testGetCellByColumnPosition()
+    {}
+
+    public function testGetCellByColumnHeaderCell()
+    {}
+
+    public function testGetCellByNonExistingColumnPositionThrowsException()
+    {}
+
+    public function testGetCellByNonExistingColumnHeaderCellThrowsException()
+    {}
+}
