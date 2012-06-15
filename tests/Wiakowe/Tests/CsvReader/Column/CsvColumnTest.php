1a69ac1c014c2a98184d54a4cfee17bbd2f5a32b
Roger Llopart Pla <lumbendil@gmail.com>
2012-06-15 09:35:05 +0200

    Added an initial test.

---
+++ tests/Wiakowe/Tests/CsvReader/Column/CsvColumnTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Wiakowe\Tests\CsvReader\Column;
+
+use Wiakowe\CsvReader\Column\CsvColumn;
+
+class CsvColumnTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider cellDataProvider
+     */
+    public function testGetAllCells(array $cellArray)
+    {
+        $csvColumn = new CsvColumn($cellArray);
+
+        $cellsFromGetCells = $csvColumn->getCells();
+
+        $this->assertInternalType('array', $cellsFromGetCells,
+            'The return must be an array.');
+
+        $this->assertContainsOnly(
+            '\Wiakowe\CsvReader\Cell\CsvCell', $cellsFromGetCells,
+            'The return from must only contain CsvCells');
+
+        foreach($cellArray as $cell) {
+            $this->assertContains($cell, $cellsFromGetCells,
+                'The getCells must contain the cells that where given to it');
+        }
+    }
+
+    public static function cellDataProvider()
+    {
+        $cell1 = \Mockery::mock('\Wiakowe\CsvReader\Cell\CsvCell');
+        $cell2 = \Mockery::mock('\Wiakowe\CsvReader\Cell\CsvCell');
+        $cell3 = \Mockery::mock('\Wiakowe\CsvReader\Cell\CsvCell');
+
+        return array(
+            array(
+                array(
+                    $cell1,
+                    $cell2,
+                    $cell3
+                )
+            ),
+            array(
+                array()
+            )
+        );
+    }
+}
