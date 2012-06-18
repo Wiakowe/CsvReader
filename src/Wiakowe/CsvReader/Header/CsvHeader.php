52b0953c52dd1f9ccc04d3b8f3b64d63ea951f3a
Roger Llopart Pla <lumbendil@gmail.com>
2012-06-18 10:50:54 +0200

    Implemented the methods required to pass the CsvHeader tests.

--- src/Wiakowe/CsvReader/Header/CsvHeader.php
+++ src/Wiakowe/CsvReader/Header/CsvHeader.php
@@ -1,25 +1,33 @@
 <?php
 namespace Wiakowe\CsvReader\Header;
 
+use Wiakowe\CsvReader\Exception\CellNotFoundException;
+
 /**
  * A row which acts as a header.
  */
 class CsvHeader
 {
+    protected $cells;
+
     /**
      * @param CsvHeaderCell[] $headerCells
      */
     public function __construct(array $headerCells)
-    {}
+    {
+        $this->cells = $headerCells;
+    }
 
     /**
-     * The total number of headers, which should be equal to the number of rows
-     * returned by <code>CsvFile::getNumRows()</code>.
+     * The total number of headers, which should be equal to the number of
+     * columns returned by <code>CsvFile::totalColumns()</code>.
      *
      * @return integer
      */
     public function getNumHeaders()
-    {}
+    {
+        return count($this->cells);
+    }
 
     /**
      * Gets a header by it's name.
@@ -27,17 +35,38 @@ class CsvHeader
      * @param string $name
      *
      * @return CsvHeaderCell
+     *
+     * @throws \Wiakowe\CsvReader\Exception\CellNotFoundException
      */
     public function getHeaderCellByName($name)
-    {}
+    {
+        foreach ($this->cells as $cell) {
+            if ($cell->getName() === $name) {
+                return $cell;
+            }
+        }
+
+        throw new CellNotFoundException;
+    }
 
     /**
      * @param integer $position
      *
      * @return CsvHeaderCell
+     *
+     * @throws \Wiakowe\CsvReader\Exception\CellNotFoundException
      */
     public function getHeaderCellByPosition($position)
-    {}
+    {
+
+        foreach ($this->cells as $cell) {
+            if ($cell->getColumn()->getColumnPosition() === $position) {
+                return $cell;
+            }
+        }
+
+        throw new CellNotFoundException;
+    }
 
     /**
      * Gets all the header cells as an array.
@@ -45,5 +74,7 @@ class CsvHeader
      * @return CsvHeaderCell[]
      */
     public function getHeaderCells()
-    {}
+    {
+        return $this->cells;
+    }
 }
