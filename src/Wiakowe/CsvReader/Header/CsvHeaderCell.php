cceca4b8da176355887929a619b83feb61d3eb2b
Roger Llopart Pla <lumbendil@gmail.com>
2012-06-18 10:33:31 +0200

    Implemented the code to pass the CsvHeaderCell tests.

--- src/Wiakowe/CsvReader/Header/CsvHeaderCell.php
+++ src/Wiakowe/CsvReader/Header/CsvHeaderCell.php
@@ -7,22 +7,32 @@ use Wiakowe\CsvReader\Column\CsvColumn;
  */
 class CsvHeaderCell
 {
+    protected $name;
+    protected $column;
+
     /**
      * @param string    $name
      * @param CsvColumn $column
      */
     public function __construct($name, CsvColumn $column)
-    {}
+    {
+        $this->name   = $name;
+        $this->column = $column;
+    }
 
     /**
      * Gets the name (contents) of the cell.
      */
     public function getName()
-    {}
+    {
+        return $this->name;
+    }
 
     /**
      * Gets the column associated with this header.
      */
     public function getColumn()
-    {}
+    {
+        return $this->column;
+    }
 }
