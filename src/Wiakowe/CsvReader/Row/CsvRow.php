4b4688b3dbd465edb225c21b9a521538e1a73fa0
Roger Llopart Pla <lumbendil@gmail.com>
2012-06-18 10:14:06 +0200

    Implemented the code to pass the tests.

--- src/Wiakowe/CsvReader/Row/CsvRow.php
+++ src/Wiakowe/CsvReader/Row/CsvRow.php
@@ -1,23 +1,41 @@
 <?php
 namespace Wiakowe\CsvReader\Row;
 
+use Wiakowe\CsvReader\Header\CsvHeaderCell;
+use Wiakowe\CsvReader\Exception\CellNotFoundException;
+
 /**
  * Row of data from a csv.
  */
 class CsvRow
 {
+    protected $position;
+    protected $cells;
+
     /**
      * @param integer                           $position
      * @param \Wiakowe\CsvReader\Cell\CsvCell[] $csvCells
      */
     public function __construct($position, array $csvCells)
-    {}
+    {
+        $this->position = $position;
+
+        foreach ($csvCells as $cell) {
+            $cell->setRow($this);
+        }
+
+        $this->cells = $csvCells;
+    }
 
     /**
      * The row position in the CSV, starting from 1.
+     *
+     * @return integer
      */
     public function getRowPosition()
-    {}
+    {
+        return $this->position;
+    }
 
     /**
      * Gets the cell on the given column.
@@ -29,5 +47,23 @@ class CsvRow
      * @throws \Wiakowe\CsvReader\Exception\CellNotFoundException
      */
     public function getCell($column)
-    {}
+    {
+        $method = 'getColumnPosition';
+        if ($column instanceof CsvHeaderCell) {
+            $method = 'getHeaderCell';
+        }
+
+        $resultCells = array_filter(
+            $this->cells,
+            function($cell) use ($column, $method) {
+                return $cell->getCsvColumn()->$method() === $column;
+            }
+        );
+
+        if (!count($resultCells)) {
+            throw new CellNotFoundException;
+        }
+
+        return array_pop($resultCells);
+    }
 }
